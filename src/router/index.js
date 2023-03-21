0a7fff867f489524bcbfc615f6acbe2d024cd1d2
Tan <tanb2012139@student.ctu.edu.vn>
2023-03-21 12:54:28 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,9 @@ import ContactBook from "@/views/ContactBook.vue";
 
 const routes = [
     {
-        path: "/",
-        name: "contactbook",
-        component: ContactBook,
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
     },
 ];
 const router = createRouter({
